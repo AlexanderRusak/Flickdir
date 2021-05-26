2b6465f794239dd22286b910410ad7d937e6e71f
AlexanderRusak <375298388602@mail.ru>
2021-05-26 11:05:28 +0300

    add

--- src/components/CardItem/helpers.ts
+++ src/components/CardItem/helpers.ts
@@ -1,17 +1,31 @@
-interface Item {
-    assets: Assets
-};
+interface Preview {
+    url: string
+}
 
 interface Assets {
     preview: Preview
 }
 
-interface Preview {
-    url: string
-}
+interface Item {
+    assets: Assets
+};
 
-export function getCurrentItem(items: Array<Item>, url: string): Object {
-    return items.find(item => item.assets.preview.url === 'sttttt');
+
+
+
+
+export function getCurrentItem(i: Array<Item>, url: string): Object {
+    const items = [
+        {
+            assets: {
+                preview: {
+                    url: ''
+                }
+            }
+        }
+    ]
+
+    return items.find(item => item.assets.preview.url === url);
 }
 
 export function removeCurrentItem(items: Array<Item>, url: string): Object {
